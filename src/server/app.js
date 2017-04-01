ab33eb69f92647fb182ba3e32ac2d6eac12c9667
ksmai <kasingmai@gmail.com>
2017-04-01 14:02:40 +0800

    Add passport authentication endpoints

--- src/server/app.js
+++ src/server/app.js
@@ -1,19 +1,51 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
 import express from 'express';
+import helmet from 'helmet';
 import mongoose from 'mongoose';
+import passport from 'passport';
 import path from 'path';
+import session from 'express-session';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
+import { authRouter } from './auth';
 import webpackDevConfig from '../../webpack.config';
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/trade-book';
+const SECRET = process.env.SECRET || 'keyboard cat';
+const SESSION_OPTS = {
+  secret: SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log(`MongoDB connected: ${MONGO_URL}`))
+  .catch(err => {
+    console.log(`Fail to connect to MongoDB(${MONGO_URL}): ${err}`);
+    process.exit(1);
+  });
 
 const app = express();
+app.use(compression());
+app.use(helmet());
+app.use(session(SESSION_OPTS));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authRouter);
+
 const compiler = webpack(webpackDevConfig);
 app.use(webpackDevMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
+
+app.use((err, req, res, next) => res.status(500).end());
+
 app.listen(PORT, () => console.log(`Development Server running on port ${PORT}`));
 
 export default app;
---
+++ src/server/auth.js
@@ -0,0 +1,108 @@
+import express from 'express';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+
+import User from './user/user.model';
+import login from './user/login';
+import signup from './user/signup';
+
+function checkCredentials(username, password, done) {
+  return login({ password, name: username })
+    .then(user => done(null, user))
+    .catch(err => {
+      const mismatch = err.message &&
+        err.message.match(/not (?:found|match)/i);
+      if(mismatch) {
+        return done(null, false);
+      }
+
+      return done(err);
+    });
+}
+
+function deserializeUser(id, done) {
+  return User
+    .findById(id)
+    .exec()
+    .then(user => {
+      if(user) {
+        return done(null, user);
+      }
+
+      throw new Error(`Cannot deserialize user: ${id}`);
+    })
+    .catch(err => done(err));
+}
+
+passport.use(new LocalStrategy(checkCredentials));
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser(deserializeUser);
+
+function authErrorHandler(err, req, res, next) {
+  if(err) {
+    return res.status(400).end();
+  }
+}
+
+function signupMiddleware(req, res, next) {
+  return signup({ name: req.body.username, password: req.body.password })
+    .then(() => next())
+    .catch(err => next(err));
+}
+
+function logoutHandler(req, res) {
+  req.logout();
+  return res.status(200).end();
+}
+
+function loginHandler(req, res) {
+  res.send('Success');
+}
+
+function meHandler(req, res) {
+  const user = req.user.toObject();
+  delete user.hash;
+  delete user.__v;
+  res.json(user);
+}
+
+function ensureLogin(req, res, next) {
+  if(req.user) {
+    return next();
+  }
+  
+  const msg = 'Login is required.';
+
+  return res.status(401).send(msg).end();
+}
+
+function ensureNoLogin(req, res, next) {
+  if(!req.user) {
+    return next();
+  }
+
+  const msg = 'Not for logged-in users.';
+
+  return res.status(400).send(msg).end();
+}
+
+const authRouter = new express.Router();
+authRouter.post('/login',
+  ensureNoLogin,
+  passport.authenticate('local'),
+  loginHandler
+);
+
+authRouter.post('/signup',
+  ensureNoLogin,
+  signupMiddleware,
+  passport.authenticate('local'),
+  loginHandler
+);
+
+authRouter.get('/me', ensureLogin, meHandler);
+authRouter.get('/logout', ensureLogin, logoutHandler);
+authRouter.use(authErrorHandler);
+
+export { authRouter, ensureLogin, ensureNoLogin };
+
