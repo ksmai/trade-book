cf0d4002cdb599c96a6b26a9235d57331da55b42
ksmai <kasingmai@gmail.com>
2017-04-02 18:51:08 +0800

    Add basic login/logout logic to navbar

--- src/client/app/nav/nav-bar.component.ts
+++ src/client/app/nav/nav-bar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { AuthService } from '../core/auth.service';
+import { User } from '../core/user';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  userStream: Observable<User>;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.userStream = this.authService.loadUser();
+  }
+
+  logout(): void {
+    this.authService.logout()
+      .subscribe(() => this.router.navigate(['/']));
+  }
 }
 
