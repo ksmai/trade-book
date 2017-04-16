b16bfad9ceb65be003eebbf9ce7cade1c65fbef8
ksmai <kasingmai@gmail.com>
2017-04-16 14:38:00 +0800

    Fix landing and navigation

--- src/client/app/landing/landing.module.ts
+++ src/client/app/landing/landing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../material/material.module';
 import { LandingComponent } from './landing.component';
 import { LandingRoutingModule } from './landing-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     LandingRoutingModule,
   ],
 
